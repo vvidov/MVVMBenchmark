b781f20786c534b066bc3d22d394d78e5cd119bd
vvidov <vvidov@gmail.com>
2025-04-06 18:08:15 +0200

    Initial commit: MVVM Benchmark project setup

---
+++ WpfAppOld/Views/PersonView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace WpfAppOld.Views
+{
+    public partial class PersonView : UserControl
+    {
+        public PersonView()
+        {
+            InitializeComponent();
+        }
+    }
+}
